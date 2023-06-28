3501e9feed6cbf4755b2221edfde7232493ace1c
Andrew McNulty <andrew.mcnulty97@gmail.com>
2023-06-28 22:24:05 +0100

    test: Tests for filter users by activity. Also update get all users test to use BeEquivalentTo

--- UserManagement.Services.Tests/UserServiceTests.cs
+++ UserManagement.Services.Tests/UserServiceTests.cs
@@ -17,7 +17,35 @@ public class UserServiceTests
         var result = service.GetAll();
 
         // Assert: Verifies that the action of the method under test behaves as expected.
-        result.Should().BeSameAs(users);
+        result.Should().BeEquivalentTo(users);
+    }
+
+    [Fact]
+    public void GetActive_WhenContextReturnsEntities_MustReturnSameEntities()
+    {
+        // Arrange: Initializes objects and sets the value of the data that is passed to the method under test.
+        var service = CreateService();
+        var users = SetupUsers();
+
+        // Act: Invokes the method under test with the arranged parameters.
+        var result = service.FilterByActive(true).ToArray().AsQueryable();
+
+        // Assert: Verifies that the action of the method under test behaves as expected.
+        result.Should().BeEquivalentTo(users.Where(p => p.IsActive));
+    }
+
+    [Fact]
+    public void GetInactive_WhenContextReturnsEntities_MustReturnSameEntities()
+    {
+        // Arrange: Initializes objects and sets the value of the data that is passed to the method under test.
+        var service = CreateService();
+        var users = SetupUsers();
+
+        // Act: Invokes the method under test with the arranged parameters.
+        var result = service.FilterByActive(false);
+
+        // Assert: Verifies that the action of the method under test behaves as expected.
+        result.Should().BeEquivalentTo(users.Where(p => !p.IsActive));
     }
 
     private IQueryable<User> SetupUsers(string forename = "Johnny", string surname = "User", string email = "juser@example.com", bool isActive = true)
